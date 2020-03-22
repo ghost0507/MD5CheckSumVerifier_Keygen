bb8cb6b645287591dad93b57061b2261cf1d6686
Ghost0507 <46000010+ghost0507@users.noreply.github.com>
2020-03-21 19:37:48 -0500

    Update chksum_keygen.py

--- chksum_keygen.py
+++ chksum_keygen.py
@@ -1,20 +1,23 @@
 #
-# * Program: MD5 Checksum Verifier 5.x
+# * Program: MD5 Checksum Verifier v5.x
 # * Release: Simple Keygen
-# * Created with: Python 3.7.x
+# * Created with: Python 3.8.x
 #
 
 import hashlib
 
-static = bytearray.fromhex("4c64664d43562d").decode()
+def getMD5(inputText):
+  return hashlib.md5(inputText.encode('utf-8')).hexdigest().upper()
+
+static = 'LdfMCV-'
 regName = input("Enter any registration name: ")
 
-if(regName==""):
+if(regName.strip()==""):
     print("Registration name can't be empty!")
 else:
-    firstHash = (hashlib.md5(regName.encode('utf-8')).hexdigest()).upper()
-    var1 = firstHash[12:14]
-    var2 = firstHash[16:27]
-    secondHash = (hashlib.md5(var2.encode('utf-8')).hexdigest()).upper()
-    var3 = secondHash[19:22]
-    print("Your registration code is: " + static + var1 + var3 + '\n')
+   md5Hash1 = getMD5(regName)
+   var1 = md5Hash1[12:14]
+   var2 = md5Hash1[16:27]
+   md5Hash2 = getMD5(var2)
+   var3 = md5Hash2[19:22]
+   print("Registration code: " + static + var1 + var3)
